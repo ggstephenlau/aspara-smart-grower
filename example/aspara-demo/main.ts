1e143cddc8cafdc95905f4447387a98e60fc5122
stephenlau <stephenlau@grow-green.com>
2023-11-29 10:09:04 +0800

    try to rename example

--- example/aspara-demo/main.ts
+++ example/aspara-demo/main.ts
@@ -1,5 +1,5 @@
 function get_date_time () {
-    date_time = asparaStylist.getDatetime()
+    date_time = asparaSmartGrower.getDatetime()
     Year = date_time[1] * 100 + date_time[0]
     month = date_time[2]
     day = date_time[3]
@@ -11,10 +11,10 @@ function toggle_pump () {
     for (let index = 0; index < 20; index++) {
         if (using_BLE == 0) {
             using_BLE = 1
-            if (asparaStylist.pumpState() == 0) {
-                asparaStylist.setPump(ON_OFF.on)
+            if (asparaSmartGrower.pumpState() == 0) {
+                asparaSmartGrower.setPump(ON_OFF.on)
             } else {
-                asparaStylist.setPump(ON_OFF.off)
+                asparaSmartGrower.setPump(ON_OFF.off)
             }
             using_BLE = 0
             break;
@@ -41,14 +41,14 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
 })
 input.onButtonPressed(Button.A, function () {
-    asparaStylist.beep(DURATION.short)
+    asparaSmartGrower.beep(DURATION.short)
     set_LED_lights(100, 0, 50)
 })
 function get_data () {
     if (using_BLE == 0) {
         using_BLE = 1
-        serial.writeValue("temperature", asparaStylist.temperature())
-        serial.writeValue("light intensity", asparaStylist.lightsensor())
+        serial.writeValue("temperature", asparaSmartGrower.temperature())
+        serial.writeValue("light intensity", asparaSmartGrower.lightsensor())
         using_BLE = 0
     }
 }
@@ -56,16 +56,16 @@ function set_LED_lights (red: number, blue: number, white: number) {
     if (using_BLE == 0) {
         using_BLE = 1
         if (red != red_intensity) {
-            asparaStylist.setLEDlight(LED_TYPE.red, red)
-            red_intensity = asparaStylist.ledIntensity(LED_TYPE.red)
+            asparaSmartGrower.setLEDlight(LED_TYPE.red, red)
+            red_intensity = asparaSmartGrower.ledIntensity(LED_TYPE.red)
         }
         if (blue != blue_intensity) {
-            asparaStylist.setLEDlight(LED_TYPE.blue, blue)
-            blue_intensity = asparaStylist.ledIntensity(LED_TYPE.blue)
+            asparaSmartGrower.setLEDlight(LED_TYPE.blue, blue)
+            blue_intensity = asparaSmartGrower.ledIntensity(LED_TYPE.blue)
         }
         if (white != white_intensity) {
-            asparaStylist.setLEDlight(LED_TYPE.white, white)
-            white_intensity = asparaStylist.ledIntensity(LED_TYPE.white)
+            asparaSmartGrower.setLEDlight(LED_TYPE.white, white)
+            white_intensity = asparaSmartGrower.ledIntensity(LED_TYPE.white)
         }
         using_BLE = 0
     }
@@ -74,17 +74,17 @@ function toggle_indicators () {
     if (using_BLE == 0) {
         using_BLE = 1
         if (toggle == 0) {
-            asparaStylist.setIndicator(INDICATOR_TYPE.indicator_5, ON_OFF.on)
+            asparaSmartGrower.setIndicator(INDICATOR_TYPE.indicator_5, ON_OFF.on)
             toggle = 1
         } else {
-            asparaStylist.setIndicator(INDICATOR_TYPE.indicator_5, ON_OFF.off)
+            asparaSmartGrower.setIndicator(INDICATOR_TYPE.indicator_5, ON_OFF.off)
             toggle = 0
         }
         using_BLE = 0
     }
 }
 input.onButtonPressed(Button.AB, function () {
-    asparaStylist.beep(DURATION.short)
+    asparaSmartGrower.beep(DURATION.short)
     toggle_pump()
 })
 function print_date_time () {
@@ -99,11 +99,11 @@ function print_date_time () {
     }
 }
 input.onButtonPressed(Button.B, function () {
-    asparaStylist.beep(DURATION.short)
+    asparaSmartGrower.beep(DURATION.short)
     set_LED_lights(0, 100, 50)
 })
 input.onGesture(Gesture.Shake, function () {
-    asparaStylist.beep(DURATION.long)
+    asparaSmartGrower.beep(DURATION.long)
 })
 let white_intensity = 0
 let blue_intensity = 0
@@ -118,14 +118,14 @@ let month = 0
 let Year = 0
 let date_time: Buffer = null
 basic.showIcon(IconNames.Heart)
-asparaStylist.startAsparaLiteBleControllerService(22582)
+asparaSmartGrower.startAsparaSmartGrowerService(22582)
 loops.everyInterval(1000, function () {
     get_date_time()
     get_data()
-    if (asparaStylist.lightsensor() < 20) {
+    if (asparaSmartGrower.lightsensor() < 20) {
         set_LED_lights(0, 0, 100)
     }
-    if (asparaStylist.lightsensor() > 1000) {
+    if (asparaSmartGrower.lightsensor() > 1000) {
         set_LED_lights(0, 0, 0)
     }
 })
