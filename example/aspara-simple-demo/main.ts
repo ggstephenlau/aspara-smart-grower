c3bc09bd9c407837c109022924ea7d1a5f8af6a9
stephenlau <stephenlau@grow-green.com>
2023-11-29 12:16:09 +0800

    update simple demo

--- example/aspara-simple-demo/main.ts
+++ example/aspara-simple-demo/main.ts
@@ -5,33 +5,36 @@ bluetooth.onBluetoothDisconnected(function () {
     basic.showIcon(IconNames.Sad)
 })
 input.onButtonPressed(Button.A, function () {
-    asparaStylist.beep(DURATION.short)
-    asparaStylist.setLEDlight(LED_TYPE.red, 100)
-    asparaStylist.setLEDlight(LED_TYPE.blue, 0)
-    asparaStylist.setLEDlight(LED_TYPE.white, 0)
+    asparaSmartGrower.beep(DURATION.short)
+    asparaSmartGrower.setLEDlight(LED_TYPE.red, 100)
+    asparaSmartGrower.setLEDlight(LED_TYPE.blue, 0)
+    asparaSmartGrower.setLEDlight(LED_TYPE.white, 0)
 })
 input.onButtonPressed(Button.AB, function () {
-    asparaStylist.beep(DURATION.short)
-    asparaStylist.setLEDlight(LED_TYPE.white, 100)
-    asparaStylist.setLEDlight(LED_TYPE.red, 0)
-    asparaStylist.setLEDlight(LED_TYPE.blue, 0)
+    asparaSmartGrower.beep(DURATION.short)
+    asparaSmartGrower.setLEDlight(LED_TYPE.white, 100)
+    asparaSmartGrower.setLEDlight(LED_TYPE.red, 0)
+    asparaSmartGrower.setLEDlight(LED_TYPE.blue, 0)
 })
 input.onButtonPressed(Button.B, function () {
-    asparaStylist.beep(DURATION.short)
-    asparaStylist.setLEDlight(LED_TYPE.blue, 100)
-    asparaStylist.setLEDlight(LED_TYPE.red, 0)
-    asparaStylist.setLEDlight(LED_TYPE.white, 0)
+    asparaSmartGrower.beep(DURATION.short)
+    asparaSmartGrower.setLEDlight(LED_TYPE.blue, 100)
+    asparaSmartGrower.setLEDlight(LED_TYPE.red, 0)
+    asparaSmartGrower.setLEDlight(LED_TYPE.white, 0)
 })
 input.onGesture(Gesture.Shake, function () {
-    asparaStylist.beep(DURATION.short)
-    if (asparaStylist.pumpState() == 0) {
-        asparaStylist.setPump(ON_OFF.on)
+    asparaSmartGrower.beep(DURATION.short)
+    if (asparaSmartGrower.pumpState() == 0) {
+        asparaSmartGrower.setPump(ON_OFF.on)
     } else {
-        asparaStylist.setPump(ON_OFF.off)
+        asparaSmartGrower.setPump(ON_OFF.off)
     }
+    asparaSmartGrower.setLEDlight(LED_TYPE.red, 0)
+    asparaSmartGrower.setLEDlight(LED_TYPE.blue, 0)
+    asparaSmartGrower.setLEDlight(LED_TYPE.white, 0)
 })
-basic.showIcon(IconNames.Heart)
-asparaStylist.startAsparaLiteBleControllerService(22582)
+basic.showIcon(IconNames.Skull)
+asparaSmartGrower.startAsparaSmartGrowerService(22582)
 basic.forever(function () {
 	
 })
