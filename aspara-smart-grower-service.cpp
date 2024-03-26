fe3e104a524b969e5f92a553bfddbf0ef5482855
stephenlau <stephenlau@grow-green.com>
2024-03-26 12:39:53 +0800

    security try

--- aspara-smart-grower-service.cpp
+++ aspara-smart-grower-service.cpp
@@ -41,7 +41,9 @@ void asparaSmartGrowerService::static_pm_events(const pm_evt_t* p_event) {
 }
 
 void asparaSmartGrowerService::pm_events(const pm_evt_t* p_event) {
-  if(p_event->evt_id == PM_EVT_PEER_DATA_UPDATE_SUCCEEDED) {
+  // if(p_event->evt_id == PM_EVT_PEER_DATA_UPDATE_SUCCEEDED) {
+  if(p_event->evt_id == PM_EVT_CONN_SEC_SUCCEEDED) {
+    ins->ubitBLEConnected = true;
     for(int i=asparaCharControlStatus, idx=0; i<asparaCharCount;i++, idx++) {
 
       // Get the CCCD
@@ -72,7 +74,7 @@ void onConnected(MicroBitEvent)
 {
   asparaSmartGrowerService *ins = asparaSmartGrowerService::getInstance();
   if (ins) {
-    ins->ubitBLEConnected = true;
+    // ins->ubitBLEConnected = true;
     ins->connectedTimeMark = system_timer_current_time();
   }
 }
