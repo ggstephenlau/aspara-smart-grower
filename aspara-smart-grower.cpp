7bb8d9d38212356f65894cc18be5c055aaa93ebd
stephenlau <stephenlau@grow-green.com>
2024-07-31 11:59:39 +0800

    v4.0.14

--- aspara-smart-grower.cpp
+++ aspara-smart-grower.cpp
@@ -47,7 +47,7 @@ namespace asparaSmartGrower {
         char IdName[] = "GGmB-000000";
 
         // snprintf(&IdName[6], 6, "%s", id->getUTF8Data());
-        snprintf(IdTemp, 20, "%d", id->getUTF8Size());
+        snprintf(IdTemp, 20, "%d%s", id->getUTF8Size(), id->getUTF8Data());
         len = strlen(IdTemp);
         if (len > 0) {
           len = (len > 5) ? 5 : len;
